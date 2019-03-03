0c7a3ecee593e5a9ca651b285d807cc2793102ef
bastara <kolobokuu@yandex.ru>
2019-03-03 17:41:01 +0700

    DZ4

--- src/DZ1/country.js
+++ src/DZ1/country.js
@@ -1,77 +1,135 @@
-var country = [russia, usa, france];
-
-
-
-
-
 var moscow = {
     name: "Moscow",
     population: 12500000
-}
+};
 
 var ufa = {
     name: "Ufa",
     population: 1100000
-}
+};
 
 var nsk = {
     name: "Nsk",
     population: 1600000
-}
+};
 
 var spb = {
     name: "Spb",
     population: 5300000
-}
+};
 
 var kazan = {
     name: "Kazan",
     population: 1200000
-}
+};
 
 var russia = {
     name: "Russia",
     city: [moscow, ufa, nsk, spb, kazan]
-}
+};
 
 var washington = {
     name: "Washington",
     population: 658000
-}
+};
 
 var chikago = {
     name: "Chikago",
     population: 2700000
-}
+};
 
 var detroit = {
     name: "Detroit",
     population: 681000
-}
+};
+
+var dallas = {
+    name: "Dallas",
+    population: 1200000
+};
+
+var ostin = {
+    name: "Ostin",
+    population: 900000
+};
 
 var usa = {
     name: "Usa",
-    city: [washington, chikago, detroit]
-}
+    city: [washington, chikago, detroit, dallas, ostin]
+};
 
 var paris = {
     name: "Paris",
     population: 2200000
-}
+};
 
 var lion = {
     name: "Lion",
     population: 500000
-}
+};
 
 var tuluza = {
     name: "Tuluza",
     population: 440000
-}
+};
 
 var france = {
     name: "France",
-    city: [washington, chikago, detroit]
+    city: [paris, lion, tuluza]
+};
+
+var country = [russia, usa, france];
+
+// •Найдите страну/страны с максимальным количеством городов
+console.log("Страны с максимальным количеством городов:");
+contryWithMaxNumberOfCity(country);
+
+
+// Получите объект с информацией по всем странам такого вида: ключ –название страны, значение –суммарная численность по стране
+var countryInfo={};
+
+getCountryInfo(country);
+console.log(countryInfo);
+
+
+
+function getCountryInfo(array) {
+    function getPopulatoin(city) {
+        var population = 0;
+        for (var i = 0; i < city.length; i++) {
+            population += city[i].population;
+        }
+        return population;
+    }
+
+    for (var i = 0; i < array.length; i++) {
+        countryInfo[array[i].name] = getPopulatoin(array[i].city);
+    }
 }
 
-var
+function contryWithMaxNumberOfCity(array) {
+    var count = 0;
+    for (i = 0; i < array.length; i++) {
+        if (count < array[i].city.length) {
+            count = array[i].city.length;
+        }
+    }
+
+    var countCity = [];
+    var n = -1;
+    for (i = 0; i < array.length; i++) {//удалил var перед i, идея ругалась, странно почему в др местах не ругается.
+        if (count === array[i].city.length) {
+            n++;
+            countCity[n] = new Array(2);
+            countCity[n][0] = array[i].name;
+            countCity[n][1] = count;
+        }
+    }
+
+    for (var i = 0; i < countCity.length; i++) {
+        console.log("Страна: " + countCity[i][0] + ". Городов: " + countCity[n][1] + ".")
+    }
+}
+
+
+
--- src/DZ1/main.js
+++ src/DZ1/main.js
@@ -1,38 +1,46 @@
 console.log("Массив чисел");
+//Создайте массив чисел
 var list = [2, 5, 6, 9, 654, 2, -65, 234];
 console.log(list);
+
+//•Отсортируйте его по убыванию
 list.sort(function (e1, e2) {
     return e2 - e1;
 });
 console.log("Сортировка по убыванию");
 console.log(list);
+
+//•Получите подмассив из первых 5 элементов и подмассив из последних 5 элементов
 console.log("Первые 5 значений");
 console.log(list.slice(0, 5));
 console.log("Последние 5 значений");
 console.log(list.slice(list.length - 5, list.length));
 
+//•Найдите сумму элементов массива, которые являются четными числами
 var sum = 0;
 list.forEach(function (item) {
     sum += even(item) ? item : 0;
 });
 
 function even(num) {
-    if (num + 1 & 1) {//не смог найти как поставить отрицание и не стал делать через else. Или через елсе вернее будет?
-        return true;
-    } else false;
+    return !!(num + 1 & 1);
 }
 
 console.log("Найдите сумму элементов массива, которые являются четными числами: " + sum);
 console.log();
 
+// •Создайте массив чисел от 1 до 100, в таком порядке
 console.log("Создайте массив чисел от 1 до 100, в таком порядке");
 var list100 = [];
 arr100(list100, 100);
 console.log(list100);
+//•Получите список квадратов четных чисел из этого массива
 console.log("Получите список квадратов четных чисел из этого массива");
-pow2(list100);
+var arrayPow2 = pow2(list100);
+console.log(arrayPow2);
 
-function arr100(arr,n) {
+
+function arr100(arr, n) {
     for (var i = 0; i < n; i++) {
         // arr.push(i + 1);
         arr[i] = i + 1;
@@ -40,9 +48,14 @@ function arr100(arr,n) {
 }
 
 function pow2(arr) {
+    var array = [];//я понимаю что массив pow2 тут так же видно но решил сделать отдельный массив, заполнить его и потом его присвоить основному массиву.
+    var count = 0;
     for (var i = 0; i < 100; i++) {
         if (even(arr[i])) {
-            console.log(arr[i] * arr[i]);//в яве не желательно использовать функции для вывода на консоль, а тут как?
+            array[count] = arr[i] * arr[i];
+            count++;
+            // console.log(array[i]);//в яве не желательно использовать функции для вывода на консоль, а тут как?
         }
     }
+    return array;
 }
